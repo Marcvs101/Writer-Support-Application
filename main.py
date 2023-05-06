ef75d0a67df534098a957835ff757af00cab25f3
Marcvs101 <marcocuoci@gmail.com>
2023-05-06 18:33:05 +0200

    stable state

---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+import re
+import json
+
+from flask import Flask
+from flask import escape
+from flask import request
+from flask import jsonify
+from flask import render_template
+from flask_cors import CORS
+
+
+
+# CONSTANTS
+DESINENZE_COMUNI = {
+    "mente"
+}
+
+
+
+## START SERVER
+app = Flask(__name__)
+CORS(app)
+
+
+
+## CONTROLLER
+# Root
+@app.get("/")
+def get_root():
+    return render_template("root.html.jinja")#, variable=variable)
+    # Use jinja2 for template
+
+
+
+# Analisi del testo
+@app.post("/analyzeText")
+def post_analyze_text():
+
+    text_input = request.form["textInput"]
+
+    text_statistics = dict()
+    worktext = text_input
+    worktext = re.sub("\\W+"," ",worktext)
+    for word in worktext.split(" "):
+        word = word.lower()
+        #print(word)
+        if word not in text_statistics:
+            text_statistics[word] = 0
+        text_statistics[word] += 1
+
+        # Desinenza comune?
+        for desin in DESINENZE_COMUNI:
+            if word.endswith(desin) and word!=desin:
+                desin = "-"+desin
+                if desin not in text_statistics:
+                    text_statistics[desin] = 0
+                text_statistics[desin] += 1
+
+    text_output = text_input.replace("\n","<br>")
+    text_dictionary = list()
+    for word in text_statistics:
+        if (text_statistics[word]>1):
+            text_dictionary.append({"key":word, "count":str(text_statistics[word])})
+    text_dictionary.sort(key=lambda i: i["count"], reverse=True)
+
+    return render_template("analyze_text.html.jinja", displayed_text=text_output, text_dictionary=text_dictionary)
---
+++ static/js/highlight_text.js
@@ -0,0 +1,18 @@
+const worktext = document.getElementById("displayed_text").innerHTML
+
+function highlightText(target){
+    let regexPattern = new RegExp("\\b"+target+"\\b", 'gi');
+
+    if(target.startsWith("-")){
+        regexPattern = new RegExp("\\w"+target.replace("-","")+"\\b", 'gi');
+    }
+
+    let text = worktext;
+    text = text.replace(/(<mark class="highlightColor">|<\/mark>)/gim, '');
+    const newText = text.replace(regexPattern, '<mark class="highlightColor">$&</mark>');
+    document.getElementById("displayed_text").innerHTML = newText;
+}
+
+function highlightTextReset(){
+    document.getElementById("displayed_text").innerHTML = worktext;
+}
