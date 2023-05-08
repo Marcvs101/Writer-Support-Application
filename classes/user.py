fe703d0a45605f91dbce5b3f7b415ba4c7a24bde
Marcvs101 <marcocuoci@gmail.com>
2023-05-08 20:06:42 +0200

    utenti

---
+++ classes/user.py
@@ -0,0 +1,140 @@
+import json
+import hashlib
+import flask_login
+
+
+
+class User(flask_login.UserMixin):
+    # Constructor
+    def __init__(self,user):
+        self.id = user["userid"]
+        self.name = user["username"]
+        self.active = user["active"]
+        self.authenticated = True
+
+
+    # Override
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def is_authenticated(self):
+        return self.authenticated and self.is_active
+    
+    @property
+    def is_anonymous(self):
+        return False
+    
+    def get_id(self):
+        return self.id
+    
+
+    # Getter
+    def get_from_id(user_id):
+        # Check if user really exists
+        f = open("keys/users.json",mode="r",encoding="utf-8")
+        userlist = json.loads(f.read())
+        f.close()
+
+        for user in userlist:
+            if user["userid"] == user_id:
+                return User(user)
+
+        return None
+
+    # Authentication
+    def authenticate_user(username, password):
+        # Check if user really exists
+        f = open("keys/users.json",mode="r",encoding="utf-8")
+        userlist = json.loads(f.read())
+        f.close()
+
+        user = None
+
+        for elem in userlist:
+            if elem["username"] == username:
+                if elem["hashed_password"] == hashlib.md5(password.encode("utf-8")).hexdigest():
+                    user = elem
+                    break
+
+        # Incorrect login
+        if user == None:
+            return None
+        
+        # Build the user
+        return_user = User(user)
+        return return_user
+
+
+    # Change password
+    def change_password(user,old_password,new_password):
+        # Check if user really exists
+        f = open("keys/users.json",mode="r",encoding="utf-8")
+        userlist = json.loads(f.read())
+        f.close()
+
+        user = None
+
+        for elem in userlist:
+            if elem["username"] == user.name:
+                if elem["hashed_password"] == hashlib.md5(old_password.encode("utf-8")).hexdigest():
+                    user = elem
+                    break
+
+        # Incorrect login
+        if user == None:
+            return False
+        
+        # Good user, change the password
+        new_userlist = list()
+        for elem in userlist:
+            if elem == user:
+                user["hashed_password"] = hashlib.md5(new_password.encode("utf-8")).hexdigest()
+                new_userlist.append(user)
+            else:
+                new_userlist.append(elem)
+
+        f = open("keys/users.json",mode="w",encoding="utf-8")
+        f.write(json.dumps(new_userlist))
+        f.close()
+
+        return True
+    
+    # Create User
+    def create_user(username,password):
+        # Check if user really exists
+        f = open("keys/users.json",mode="r",encoding="utf-8")
+        userlist = json.loads(f.read())
+        f.close()
+
+        user = None
+        chosen_userid = 1
+
+        for elem in userlist:
+            if elem["userid"] == chosen_userid:
+                chosen_userid += 1
+            if elem["username"] == username:
+                user = elem
+                break
+
+        # User already exists login
+        if user != None:
+            return False
+        
+        # New user, add it to the database
+        new_userlist = userlist
+        new_user = dict()
+        new_user["userid"] = chosen_userid
+        new_user["active"] = True
+        new_user["username"] = username
+        new_user["hashed_password"] = hashlib.md5(password.encode("utf-8")).hexdigest()
+        new_userlist.append(user)
+
+        new_userlist.sort(key=lambda i: int(i["userid"]), reverse=False)
+
+        f = open("keys/users.json",mode="w",encoding="utf-8")
+        f.write(json.dumps(new_userlist))
+        f.close()
+
+        return True
--- main.py
+++ main.py
@@ -4,25 +4,100 @@ import json
 import hashlib
 import datetime
 
-from flask import Flask
-from flask import escape
-from flask import request
-from flask import jsonify
-from flask import render_template
-from flask_cors import CORS
+import flask
+import flask_cors
+import flask_login
+
+from classes.user import User
 
 
 
-# CONSTANTS
+## CONSTANTS
 DESINENZE_COMUNI = {
     "mente"
 }
 
 
 
+## KEYS
+f = open("keys/secretkey.txt",mode="r",encoding="utf-8")
+skey = f.read().strip()
+f.close()
+
+
+
 ## START SERVER
-app = Flask(__name__)
-CORS(app)
+app = flask.Flask(__name__)
+app.secret_key = skey.encode('UTF-8')
+
+flask_cors.CORS(app)
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+
+
+## USER MANIPULATION
+# Load user
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_from_id(user_id)
+
+
+# Login and logout
+@app.post("/login")
+def login():
+    # Grab login form
+    login_form = flask.request.form
+
+    username = login_form["username"]
+    password = login_form["password"]
+
+    # Validate username and password
+    user = User.authenticate_user(username,password)
+    if user == None:
+        return flask.abort(400)
+
+    # Finalize login
+    flask_login.login_user(user)
+    return flask.redirect(flask.url_for('get_root'))
+
+@app.post("/logout")
+@flask_login.login_required
+def logout():
+    flask_login.logout_user()
+    return flask.redirect(flask.url_for('get_root'))
+
+@app.post("/set_password")
+@flask_login.login_required
+def set_password():
+    # Grab login form
+    login_form = flask.request.form
+
+    old_password = login_form["old_password"]
+    new_password = login_form["new_password"]
+
+    result = User.change_password(flask_login.current_user,old_password,new_password)
+    if result == True:
+        return "OK",200
+    else:
+        return flask.abort(401)
+
+
+@app.get("/users/<user_id>")
+def get_userpage(user_id):
+
+    # Logged in?
+    logged_in = False
+    user_info = None
+    if flask_login.current_user.is_authenticated:
+        logged_in = True
+        user_info = flask_login.current_user
+
+    if logged_in:
+        return flask.render_template("private_userpage.html.jinja",user_info=user_info)
+    else:
+        return flask.abort(401)
 
 
 
@@ -30,16 +105,23 @@ CORS(app)
 # Root
 @app.get("/")
 def get_root():
-    return render_template("root.html.jinja")#, variable=variable)
-    # Use jinja2 for template
 
+    # Logged in?
+    logged_in = False
+    user_info = None
+    if flask_login.current_user.is_authenticated:
+        logged_in = True
+        user_info = flask_login.current_user
+
+    return flask.render_template("root.html.jinja",logged_in=logged_in,user_info=user_info)
+    # Use jinja2 for template
 
 
 # Analisi del testo
 @app.post("/analyzeText")
 def post_analyze_text():
 
-    text_input = request.form["textInput"]
+    text_input = flask.request.form["textInput"]
 
     text_statistics = dict()
     worktext = text_input
@@ -67,11 +149,11 @@ def post_analyze_text():
     text_dictionary.sort(key=lambda i: int(i["count"]), reverse=True)
 
     f=open(file="logs/"+str(hashlib.md5(text_input.encode('utf-8')).hexdigest())+".txt",mode="w",encoding="utf-8")
-    f.write(str(request.remote_addr))
+    f.write(str(flask.request.remote_addr))
     f.write("\n")
     f.write(str(datetime.datetime.now()))
     f.write("\n----\n")
     f.write(str(text_input.replace("\r","\n").replace("\n\n","\n")))
     f.close()
 
-    return render_template("analyze_text.html.jinja", displayed_text=text_output, text_dictionary=text_dictionary)
+    return flask.render_template("analyze_text.html.jinja", displayed_text=text_output, text_dictionary=text_dictionary)
